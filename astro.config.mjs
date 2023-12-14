9d5ed04b9cad4cdcb83bf70dffabfc31b8b585ff
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2023-12-14 17:29:49 +1100

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
